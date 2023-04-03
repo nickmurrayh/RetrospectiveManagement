85e9176020ea6871727773053bee8aab24cde1f0
Nick Murray <nickmurrayh@gmail.com>
2023-04-03 15:27:19 +0100

    Added domain models and entity framework

--- retrospectives-api/retrospectives-api/Models/FeedbackItem.cs
+++ retrospectives-api/retrospectives-api/Models/FeedbackItem.cs
@@ -2,5 +2,20 @@
 
 public class FeedbackItem
 {
-    
+    public int Id { get; set; }
+    public string CreatedBy { get; set; }
+    public string? Body { get; set; }
+    public FeedbackType Type { get; set; }
+    public virtual Retrospective Retrospective { get; set; }
+    public string? RetrospectiveName { get; set; }
 }
+
+public enum FeedbackType
+{
+    Positive,
+    Negative,
+    Idea,
+    Praise
+}
+
+
--- retrospectives-api/retrospectives-api/Models/Retrospective.cs
+++ retrospectives-api/retrospectives-api/Models/Retrospective.cs
@@ -2,5 +2,11 @@
 
 public class Retrospective
 {
+    public string Name { get; set; }
+    public string? Summary { get; set; }
+    public DateTime Date { get; set; }
+    public List<string> Participants { get; set; }
+    public ICollection<FeedbackItem>? FeedbackItems { get; set; }
+    
     
 }
